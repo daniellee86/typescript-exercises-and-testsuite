bab5ff2396de5319604ae3989d7473745472c895
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-09-13 18:43:11 +0100

    solution two

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,9 +1,24 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
-  
+
+//both of these are perfectly fine solutions and can replace: {first: number, second: number} 
+
+// type Params = {
+//   first: number;
+//   second: number;
+// }
+
+// interface AddTwoNumbersArgs {
+//   first: number;
+//   second: number;
+// }
+
+
+//function with in-line typechecking 
+export const addTwoNumbers = (params: {first: number, second: number}) => {
   return params.first + params.second;
 };
+
 
 it("Should add the two numbers together", () => {
   expect(
