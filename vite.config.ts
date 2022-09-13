679a8480e9110e7909c8401a7d392b7d3ca0ea59
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-09-13 14:54:12 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    include: ["src/**/*.ts"],
+    setupFiles: ["scripts/setup.ts"],
+    passWithNoTests: true,
+  },
+});
